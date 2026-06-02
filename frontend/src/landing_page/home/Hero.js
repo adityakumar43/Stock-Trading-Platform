d6f0fb620d1bc9507c643a6d5436c3683a697f83
Aditya <kumaradityap123@gmail.com>
2026-06-02 13:20:03 +0530

    modified hero

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -4,7 +4,7 @@ function Hero() {
         <div className='container p-5 mb-5'>
             <div className='row text-center'>
                 <img src='images/homeHero.png' alt='homeHero' className='mb-5'/>
-                <h1 className='mt-5'>Invest in everything</h1>
+                <h3 className='mt-5'>Invest in everything</h3>
                 <p>One platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
                 <button className='p-2 btn btn-primary fs-5' style={{width:"20%", margin:"0 auto"}}>Sign Up for Free</button>
             </div>
