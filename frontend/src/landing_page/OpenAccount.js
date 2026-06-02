2d08d85f16f00d5914363c931552c157d5d4aaeb
Aditya <kumaradityap123@gmail.com>
2026-06-02 13:42:46 +0530

    modified openAccount

--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -4,9 +4,9 @@ function OpenAccount() {
     return (
         <div className='container p-5 mb-5'>
             <div className='row text-center'>
-                <h1 className='mt-5'>Open a Zerodha account</h1>
-                <p>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
-                <button className='p-2 btn btn-primary fs-5' style={{width:"20%", margin:"0 auto"}}>Sign Up for Free</button>
+                <h3 className='mt-5'>Open a Zerodha account</h3>
+                <p className='mt-3'>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
+                <button className='p-2 btn btn-primary fs-5 mt-4' style={{width:"20%", margin:"0 auto"}}>Sign Up for Free</button>
             </div>
         </div>
     );
